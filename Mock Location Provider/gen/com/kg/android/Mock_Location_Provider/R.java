05e492f780b14bebc6f8428d283575870a91ad2c
gautam714 <gautam714@gmail.com>
2014-11-20 13:05:21 +0530

    Revert "Mock Location Provider"
    This reverts commit c18f435fa93abea81630d036a1f44adcbf41c9d8.

--- Mock Location Provider/gen/com/kg/android/Mock_Location_Provider/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.kg.android.Mock_Location_Provider;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
